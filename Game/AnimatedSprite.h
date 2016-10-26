86e4c6f121820caf5e478d0493f0335fd8a36b8d
vik tallo <victor.tallo@gmail.com>
2016-10-26 23:55:36 +0200

    - Cambiado el tamaño del frameList[5] a frameList[4]

--- Game/AnimatedSprite.h
+++ Game/AnimatedSprite.h
@@ -9,7 +9,7 @@ private:
 	int sizeX;							///< Tamaño X de cada frame.
 	int sizeY;							///< Tamaño Y de cada frame.
 	sf::Clock clock;					///< Reloj del juego.
-	int frameList[5] = { 0, 1, 2, 1 };	///< La animación completa se compone de un ciclo entero de las animaciones: inicio - final - inicio.
+	int frameList[4] = { 0, 1, 2, 1};	///< La animación completa se compone de un ciclo entero de las animaciones: inicio - final - inicio.
 	int frameActual = 0;				///< Contador de frames.
 	int direccion = 0;					///< Direccion 0-4: 0 = SUR, 1 = OESTE, 2 = ESTE, 3 = NORTE.
 public:
